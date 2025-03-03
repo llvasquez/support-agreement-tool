2e10ee348f5989e092bbe9a3fda22ccbb4c693bf
Lauren Vasquez <laurenvasquez@Laurens-MacBook-Pro.local>
2025-03-02 22:56:21 -0500

    updated template

--- src/data/agreementTemplates.ts
+++ src/data/agreementTemplates.ts
@@ -9,29 +9,81 @@ export const MOA_TEMPLATE: Template = {
   lastUpdated: new Date('2025-02-01'),
   sections: [
     {
-      id: 'purpose',
-      name: 'Purpose',
-      content: 'This Memorandum of Agreement (MOA) establishes the terms and conditions under which [Supporting DoD Component] will provide support to [Supported DoD Component] in accordance with DoDI 4000.19.',
+      id: 'introduction',
+      name: 'Introduction',
+      content: 'This is a memorandum of agreement (MOA) between the [First Party] and the [Second Party][if the Second Party is a non-governmental entity, include its address]. When referred to collectively, the [First Party] and the [Second Party] are referred to as the “Parties.',
       isMandatory: true,
       classificationMarking: ''
     },
     {
-      id: 'scope',
-      name: 'Scope',
+      id: 'background',
+      name: 'Background',
+      content: 'If there is a need to discuss background, do so here. Normally, there is no need to discuss the background or provide justification for the MOA, particularly if between OSD or DoD Components. Occasionally, however, there is a desire to explain the need for the MOA; particularly where it is not self-evident from the purpose or it is with another federal agency.',
+      isMandatory: false,
+      classificationMarking: ''
+    },
+    {
+      id: 'authorities',
+      name: 'Authorities',
+      content: 'State the legal authority upon which the reimbursable MOA is based, such as the Economy Act, or any other legal or significant authority that authorizes any such actions associated with this MOA. Discuss the authorities of the Parties here. If the other Party is another federal agency and insists on stating what it believes to be its own authority, preface that assertion with “The [Party] asserts the following authority:” The DoD has no obligation to agree with such assertions of authority by other federal agencies.]',
+      isMandatory: true,
+      classificationMarking: ''
+    },
+    {
+      id: 'purpose and scope',
+      name: 'Purpose and Scope',
       content: 'This agreement covers the following support: [Detailed description of the support to be provided].',
       isMandatory: true,
       classificationMarking: ''
     },
     {
-      id: 'responsibilities',
-      name: 'Responsibilities',
-      content: `a. [Supporting DoD Component] Responsibilities:
+      id: 'responsibilities of the parties',
+      name: 'Responsibilities of the Parties',
+      content: `a. [First Party] Responsibilities:
 1. [List specific responsibilities]
 2. [List specific responsibilities]
 
-b. [Supported DoD Component] Responsibilities:
+b. [Second Party] Responsibilities:
 1. [List specific responsibilities]
 2. [List specific responsibilities]`,
+      isMandatory: true,
+      classificationMarking: ''
+    },
+    {
+      id: 'personnel',
+      name: 'Personnel',
+      content: '',
+      isMandatory: true,
+      classificationMarking: ''
+    },
+    {
+      id: 'Points of Contact',
+      name: 'Points of Contact (POCs)',
+      content: `The following POCs will be used by the Parites to communicate matters concerning this MOA. Each party may chnage itsPOC upon reasonable notice to the other party.
+a. For the [First Party] 
+  1. [Name, position, office identification, phone number and email of primary POC]
+  2. [Name, position, office identification, phone number and email of alternate POC]
+
+b. For the [Second Party] 
+  1. [Name, position, office identification, phone number and email of primary POC]
+  2. [Name, position, office identification, phone number and email of alternate POC]`,
+      isMandatory: true,
+      classificationMarking: ''
+    },
+    {
+      id: 'correspondence',
+      name: 'Correspondence',
+      content: `All correspondence to be sent and notices to be given pursuant to this MOA will be addressed, if to the [First party], to:
+      a. [Insert mailing address] 
+b. and, if to the  [Second Party] 
+      1. [Insert mailing address]`,
+      isMandatory: true,
+      classificationMarking: ''
+    },
+    {
+      id: 'review of agreement',
+      name: 'Review of Agreement',
+      content: 'If non-reimbursable, this MOA will be reviewed no less often than mid-point on or around the anniversary of its effective date in its entirety. If reimbursable, this MOA will be reviewed on or around the anniversary of its effective date annually for financial impacts; if there are substantial changes in resource requirements, the agreement will be reviewed in its entirety..',
       isMandatory: true,
       classificationMarking: ''
     },
